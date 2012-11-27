22298243fb02f63639fe7119164ccd8413f17d44
startling <tdixon51793@gmail.com>
2012-11-27 18:20:30 -0500

    set up the stack, too

--- boot/src/start.asm
+++ boot/src/start.asm
@@ -3,10 +3,17 @@
 global start
 start:
         ;; Get the registers in a known state.
-        xor ax, ax         ; By first clearing ax.
-        mov es, ax         ; And setting es from it.
-        mov ds, ax         ; ds, too.
-        cld                ; Clear the direction flag.
+        xor ax, ax     ; First, clear ax.
+        mov es, ax     ; Set es from it.
+        mov ds, ax     ; Set ds from it, too.
+        cld            ; Clear the direction flag.
+        ;; Set up the stack growing down from 0x7c00.
+        ;; Supposedly there's 30KiB here, free for use.
+        ;; http://wiki.osdev.org/Memory_Map_(x86)
+        cli            ; Turn of interrupts temporarily.
+	    mov sp, 0x7c00 ; Starts at 0x7c00.
+        mov ss, ax     ; Set the high byte to 0 via ax.
+        sti            ; Turn interrupts back on.
 
 ;;; Halt forever.
 global halt

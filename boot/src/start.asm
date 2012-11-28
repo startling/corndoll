aa9b13a89aa550416fcd406a7745ca9e457c6a15
startling <tdixon51793@gmail.com>
2012-11-28 15:07:51 -0500

    Cleaned up that code and commented it more thoroughly.

--- boot/src/start.asm
+++ boot/src/start.asm
@@ -2,21 +2,24 @@
 
 global start
 start:
-        ;; Get the registers in a known state.
-        xor ax, ax     ; First, clear ax.
-        mov es, ax     ; Set es from it.
-        mov ds, ax     ; Set ds from it, too.
-        cld            ; Clear the direction flag.
-        ;; Set up the stack growing down from 0x7c00.
-        ;; Supposedly there's 30KiB here, free for use.
-        ;; http://wiki.osdev.org/Memory_Map_(x86)
-        cli            ; Turn of interrupts temporarily.
-	    mov sp, 0x7c00 ; Starts at 0x7c00.
-        mov ss, ax     ; Set the high byte to 0 via ax.
-        sti            ; Turn interrupts back on.
+        cli                     ; Disable interrupts for initialization.
+        xor ax, ax              ; Zero out AX.
+        cld                     ; Clear the direction flag.
+;;; ==================================================================
+;;; Quick introduction to my understanding of real mode segmented
+;;; addressing: SS:SP represents the top of the stack, meaning the
+;;; linear address you get by multiplying the segment selector (SS in
+;;; this case) by 16 and adding that to the offset (SP).
+;;; 
+;;; tl;dr: The linear address of the stack is SS * 16 + SP.
+;;; ==================================================================
+;;; There's some space (30 KiB) immediately before 0x7c00 that we can
+;;; use for the stack. See: http://wiki.osdev.org/Memory_Map_(x86)
+        mov ss, ax              ; Zero out SS and set SP such that
+        mov sp, 0x7c00          ; SS * 16 + SP == 0x7c00.
+        sti                     ; We can reenable interrupts now.
 
-;;; Halt forever.
-global halt
+;;; Halt forever -- shut off interrupts and then hlt.
 halt:
+        cli
         hlt
-        jmp halt

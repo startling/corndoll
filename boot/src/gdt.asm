2260f1f4944c2388b94209114b18a0e191f659fc
startling <tdixon51793@gmail.com>
2012-11-28 21:03:10 -0500

    Some nice macros for constructing gdt descriptors and entries.

---
+++ boot/src/gdt.asm
@@ -0,0 +1,27 @@
+;;; ==================================================================
+;;; Some nice macros for GDT entries and descriptors.
+;;; ==================================================================
+%define gdtSize 8
+;;; ==================================================================
+%macro gdtDescriptor 2
+;;; ==================================================================
+;;; Create a gdt descriptor, given the number of entries and the
+;;; linear address to the gdt's location.
+        dw (%1 * gdtSize - 1)
+        dd %2
+%endmacro
+;;; ==================================================================
+%macro gdtEntry 3
+;;; ==================================================================
+;;; Create a descriptor table entry, given its limit, base, access
+;;; flags, and granularity bit.
+        dw (%1 & 0xffff)        ; The low word of the limit...
+        dw (%2 & 0xffff)        ; and the low word of the base.
+        db ((%2 >> 16) & 0xff)  ; Then the middle byte of the base...
+        db %3                   ; and the access byte.
+        ;; The high four bits of the limit and the granularity bit.
+        db (((%1 >> 16) & 0xf) | (%4 & 0xf0))
+        ;; And finally the highest byte of the base.
+        db ((%2 >> 24) & 0xff)
+%endmacro
+;;; ==================================================================

551612295cfcb08791947937ae510fc4546d921f
startling <tdixon51793@gmail.com>
2012-11-29 02:56:26 -0500

    Set up the stack in protected mode.

--- boot/src/start.asm
+++ boot/src/start.asm
@@ -26,18 +26,6 @@ start:
         mov gs, ax
         cld                     ; Clear the direction flag.
 ;;; ==================================================================
-;;; Quick introduction to my understanding of real mode segmented
-;;; addressing: SS:SP represents the top of the stack, meaning the
-;;; linear address you get by multiplying the segment selector (SS in
-;;; this case) by 16 and adding that to the offset (SP).
-;;; 
-;;; tl;dr: The linear address of the stack is SS * 16 + SP.
-;;; ==================================================================
-;;; There's some space (30 KiB) immediately before 0x7c00 that we can
-;;; use for the stack. See: http://wiki.osdev.org/Memory_Map_(x86)
-        mov ss, ax              ; Zero out SS and set SP such that
-        mov sp, 0x7c00          ; SS * 16 + SP == 0x7c00.
-;;; ==================================================================
  ;;; Now we set up the Global Descriptor Table, below.
         lgdt [gdtPtr]
 ;;; ==================================================================
